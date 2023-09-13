a7507eb81481acfb6f6c4c43f191b797cdab2c04
Benhooky <egorkulygin444@gmail.com>
2023-09-13 18:56:42 +0300

    Update updateRelationsForTeacherStudents.py
    Added docstrings

--- scripts/updateRelationsForTeacherStudents.py
+++ scripts/updateRelationsForTeacherStudents.py
@@ -1,9 +1,13 @@
-import random
+
 import mysql.connector
+import random
 from itertools import cycle
 
 
 def update_relations():
+    """
+    Update the student-teacher relationships in the database.
+    """
     # Establish a connection to the database
     db_connection = mysql.connector.connect(
         host="localhost",
@@ -11,28 +15,28 @@ def update_relations():
         password="egor21412SFAW",
         database="ege_online_school"
     )
-
-    # Create a cursor object to execute SQL queries
     cursor = db_connection.cursor()
 
+    # Retrieve all student IDs from the 'students' table
     cursor.execute("SELECT student_id FROM students")
     student_ids = [row[0] for row in cursor.fetchall()]
 
+    # Retrieve all teacher IDs and subject IDs from the 'teachers' table
     cursor.execute("SELECT teacher_id, subject_id FROM teachers")
     teacher_data = cursor.fetchall()
 
-    # Count the total number of distinct subjects
-    cursor.execute(
-        "SELECT subject_id FROM subjects"
-    )
+    # Retrieve all subject IDs from the 'subjects' table
+    cursor.execute("SELECT subject_id FROM subjects")
     subject_ids = [row[0] for row in cursor.fetchall()]
+
+    # Create a dictionary to map subject IDs to an iterator of teacher IDs
     subject_teacher_dict = {}
     for subject_id in subject_ids:
         teachers_for_subject = [teacher[0]
                                 for teacher in teacher_data if teacher[1] == subject_id]
         subject_teacher_dict[subject_id] = cycle(teachers_for_subject)
 
-    # Iterate over each student
+    # Update the student-teacher relationships for each student
     for student_id in student_ids:
         selected_subjects = random.sample(
             subject_ids, random.randint(1, len(subject_ids)))
@@ -45,7 +49,7 @@ def update_relations():
         # Commit the changes to the database
         db_connection.commit()
 
-    # Close the cursor and database connection
+    # Close the cursor and the database connection
     cursor.close()
     db_connection.close()
 
